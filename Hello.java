6fed1417538f2b169bf1523037312c864e19bd74
unknown <gayathridh23@gmail.com>
2024-04-02 17:36:22 +0530

    code updated

--- Hello.java
+++ Hello.java
@@ -1,45 +1,6 @@
 import java.util.*;
-class Linked {
-    Node head;
-    Node tail;
-    int size;
-    class Node{
-        int data;
-        Node next;
-        Node(int data){
-            this.data = data;
-        }
-    }
-    public void insertEnd(int data){
-        Node node = new Node(data);
-        if(head == null){
-            head = tail = node;
-            size++;
-            return;
-        }
-        tail.next = node;
-        tail = node;
-        size++;
-    }
-    public void display(int a){
-        Node temp = head;
-        for(int i = 1; i<a ; i++){
-            temp = temp.next;
-        }
-        System.out.println(temp.data);
-    }
-}
-
-class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Linked l = new Linked();
-        for(int i = 0; i<n; i++){
-            l.insertEnd(sc.nextInt());
-        }
-        int a = sc.nextInt();
-        l.display(a);
-        sc.close();
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hii bro");
     }
 }
